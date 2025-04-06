7ba7aeb4b60bcb9c747f0a24fd87402cf0197549
Griffin Guthrie <gguthrie0927@gmail.com>
2025-04-06 13:59:28 -0400

    Commiting a better algorithms

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from data_processor import DataProcessor
 from algorithms import WindSpeedPredictor, HurricaneClustering, HurricaneAnomalyDetector
 from visualizer import Visualizer 
@@ -12,10 +13,12 @@ def main():
   pd.set_option('display.max_rows', None)  # Show all rows
   pd.set_option('display.max_columns', None)  # Shows all columns
   print(clean_data)
-  X_train, y_train = processor.get_features_and_target()
+  x, y = processor.get_features_and_target()
+  X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
 # prints the first 5 features or targets for testing
-  print("Features: ", X_train[:15])
-  print("Target:", y_train)
+  print("Features: ", x[:15])
+  print("Target:", y)
 
 # Temperature prediction
 
@@ -30,24 +33,24 @@ def main():
   model = WindSpeedPredictor()
 
   model.fit(X_train, y_train)
-  print("Wind Speed: ", model.predict(X_train))
+  print("Wind Speed: ", model.predict(X_test))
 
   clustering = HurricaneClustering(n_clusters=2)
   clustering.fit(X_train)
-  print("Clustering: ",clustering.predict(X_train))
+  print("Clustering: ",clustering.predict(X_test))
 
   n_neighbors = min(10, len(X_train) - 1)
   anomaly_detector = HurricaneAnomalyDetector(contamination=0.2)
   anomaly_detector.model.set_params(n_neighbors=n_neighbors)
   anomaly_detector.fit(X_train)
-  print("Anomalies: ", anomaly_detector.predict(X_train))
+  print("Anomalies: ", anomaly_detector.predict(X_test))
 
   #Anomalies graph
-  Visualizer.plot_anomalies(X_train, anomaly_detector.predict(X_train))
+  Visualizer.plot_anomalies(X_train, anomaly_detector.predict(X_test))
   #clustering graph
-  Visualizer.plot_clustered_data(X_train, clustering.predict(X_train))
+  Visualizer.plot_clustered_data(X_test, clustering.predict(X_test))
   #Predictions Graph
-  Visualizer.plot_windspeed_trend(X_train,model.predict(X_train))
+  Visualizer.plot_windspeed_trend(X_test,model.predict(X_test))
 
 
 
